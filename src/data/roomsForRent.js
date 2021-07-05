0a363733ba46b4dc0bd839cd7aa5be0fe10ded5a
Sivaprasad S <unni007prasad@gmail.com>
2021-07-06 00:00:03 +0530

    Dummy room details added

---
+++ src/data/roomsForRent.js
@@ -0,0 +1,49 @@
+const rooms = [
+    {
+        id: '10001',
+        bhk: '3',
+        location: 'bangalore',
+        address: 'madiwala street 672342',
+        rent: '25000',
+        subleased: true,
+        owner_name: 'Kuttappan'
+    },
+    {
+        id: '10002',
+        bhk: '3',
+        location: 'Kozhkode',
+        address: 'kundan street 672332',
+        rent: '5000',
+        subleased: true,
+        owner_name: 'kundappan'
+    },
+    {
+        id: '10003',
+        bhk: '5',
+        location: 'Hyderabad',
+        address: 'kundanahalli street 672342',
+        rent: '15000',
+        subleased: false,
+        owner_name: 'Kuttappan reddy'
+    },
+    {
+        id: '10004',
+        bhk: '3',
+        location: 'Kollam',
+        address: 'fury street 672342',
+        rent: '10',
+        subleased: true,
+        owner_name: 'labhesh menon'
+    },
+    {
+        id: '10005',
+        bhk: '3',
+        location: 'Trivandrum',
+        address: 'Pinapple street 672222',
+        rent: '2500',
+        subleased: true,
+        owner_name: 'Kuttappy'
+    },
+]
+
+export default rooms;
