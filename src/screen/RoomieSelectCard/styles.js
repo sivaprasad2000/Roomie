347605d8c44007657aee5578d1458867de8ba9ef
ivin-3-69 <ivin.kuriakose.2000@gmail.com>
2021-07-07 13:20:46 +0530

    dqweewf

--- src/screen/RoomieSelectCard/styles.js
+++ src/screen/RoomieSelectCard/styles.js
@@ -7,30 +7,59 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+const win = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFFFFF',
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-    padding: 20,
-  },
 
-  logo: {
-    width: 300,
-    height: 500,
+    padding: 10,
   },
-  buttonTextNormal: {
-    fontFamily: 'Righteous-Regular',
-    fontSize: 40,
+  pictureContainer: {
+    margin: 20,
+    width: '90%',
+    height: '65%',
+    alignSelf: 'center',
   },
-  buttonUpper: {
-    backgroundColor: '#fff',
-    width: '30%',
-    height: '30%',
+  picture: {
+    width: '100%',
+    height: '100%',
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  textContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+  },
+  nameeeText: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  descText: {
+    fontSize: 20,
+  },
+  buttonXcontainer: {
+    backgroundColor: 'red',
+    width: '10%',
+    height: 40,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 10,
-    paddingLeft: 10,
+  },
+  buttonOcontainer: {
+    marginTop: 20,
+    backgroundColor: 'blue',
+    width: '10%',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  buttoncontainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
